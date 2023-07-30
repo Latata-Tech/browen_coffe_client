69819f69bf3b03147f56bc1df5c5fe4aa0afcc5f
Han <han@scallop.io>
2023-07-31 02:37:28 +0700

    feat: update

--- lib/page/update_selling.dart
+++ lib/page/update_selling.dart
@@ -54,163 +54,165 @@ class _UpdateSellingState extends State<UpdateSelling> {
       appBar: AppBar(
         title: const Text('Update Pesanan'),
       ),
-      body: Center(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width / 1.2,
-          child: Column(
-            children: [
-              SizedBox(
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height / 2,
-                child: Padding(
-                  padding: EdgeInsets.all(10),
-                  child: FutureBuilder(
-                    future: futureOrder,
-                    builder: (BuildContext context, AsyncSnapshot<Order?>snapshot) {
-                      print(snapshot.hasData);
-                      if(snapshot.hasData) {
-                        total = [];
-                        quantity = [];
-                        for (var element in snapshot.data!.orderDetail) {
-                          total.add(element.qty * (element.price ?? 0));
-                        }
-                        return ListView.builder(
-                          itemCount: snapshot.data!.orderDetail.length,
-                          itemBuilder: (context, index) {
-                            quantity.add(snapshot.data!.orderDetail[index]);
-                            return Container(
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(snapshot.data!.orderDetail[index].name),
-                                  SizedBox(
-                                    height: 20,
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.start,
-                                      children: [
-                                        SizedBox(
-                                          width: 40,
-                                          child: OutlinedButton(
-                                            onPressed: () {
-                                              if (quantity[index].qty != 0) {
+      body: SingleChildScrollView(
+        child: Center(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width / 1.2,
+            child: Column(
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height / 2,
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: FutureBuilder(
+                      future: futureOrder,
+                      builder: (BuildContext context, AsyncSnapshot<Order?>snapshot) {
+                        print(snapshot.hasData);
+                        if(snapshot.hasData) {
+                          total = [];
+                          quantity = [];
+                          for (var element in snapshot.data!.orderDetail) {
+                            total.add(element.qty * (element.price ?? 0));
+                          }
+                          return ListView.builder(
+                            itemCount: snapshot.data!.orderDetail.length,
+                            itemBuilder: (context, index) {
+                              quantity.add(snapshot.data!.orderDetail[index]);
+                              return Container(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(snapshot.data!.orderDetail[index].name),
+                                    SizedBox(
+                                      height: 20,
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        children: [
+                                          SizedBox(
+                                            width: 40,
+                                            child: OutlinedButton(
+                                              onPressed: () {
+                                                if (quantity[index].qty != 0) {
+                                                  setState(() {
+                                                    var data = quantity[index].qty - 1;
+                                                    quantity[index].qty--;
+                                                    total[index] = data * (quantity[index].price ?? 0);
+                                                  });
+                                                }
+                                              },
+                                              style: OutlinedButton.styleFrom(
+                                                  side: const BorderSide(
+                                                      width: 1.0, color: Colors.black)),
+                                              child: const Icon(Icons.remove_circle,
+                                                  color: Colors.black, size: 11,),
+                                            ),
+                                          ),
+                                          Container(
+                                            decoration: BoxDecoration(
+                                                borderRadius: BorderRadius.circular(8),
+                                                border: Border.all(color: Colors.black)),
+                                            width: 50,
+                                            height: 20,
+                                            child: Center(child: Text(quantity[index].qty.toString())),
+                                          ),
+                                          SizedBox(
+                                            width: 40,
+                                            child: OutlinedButton(
+                                              onPressed: () {
                                                 setState(() {
-                                                  var data = quantity[index].qty - 1;
-                                                  quantity[index].qty--;
+                                                  var data = quantity[index].qty + 1;
+                                                  quantity[index].qty++;
                                                   total[index] = data * (quantity[index].price ?? 0);
                                                 });
-                                              }
-                                            },
-                                            style: OutlinedButton.styleFrom(
+                                              },
+                                              style: OutlinedButton.styleFrom(
                                                 side: const BorderSide(
-                                                    width: 1.0, color: Colors.black)),
-                                            child: const Icon(Icons.remove_circle,
-                                                color: Colors.black, size: 11,),
-                                          ),
-                                        ),
-                                        Container(
-                                          decoration: BoxDecoration(
-                                              borderRadius: BorderRadius.circular(8),
-                                              border: Border.all(color: Colors.black)),
-                                          width: 50,
-                                          height: 20,
-                                          child: Center(child: Text(quantity[index].qty.toString())),
-                                        ),
-                                        SizedBox(
-                                          width: 40,
-                                          child: OutlinedButton(
-                                            onPressed: () {
-                                              setState(() {
-                                                var data = quantity[index].qty + 1;
-                                                quantity[index].qty++;
-                                                total[index] = data * (quantity[index].price ?? 0);
-                                              });
-                                            },
-                                            style: OutlinedButton.styleFrom(
-                                              side: const BorderSide(
-                                                  width: 1, color: Colors.black),
+                                                    width: 1, color: Colors.black),
+                                              ),
+                                              child: const Icon(Icons.add_circle,
+                                                  color: Colors.black, size: 11),
                                             ),
-                                            child: const Icon(Icons.add_circle,
-                                                color: Colors.black, size: 11),
                                           ),
-                                        ),
-                                      ],
+                                        ],
+                                      ),
                                     ),
-                                  ),
-                                  Text(Converter.currencyIndonesia(quantity[index].qty * (snapshot.data!.orderDetail[index].price ?? 0)))
-                                ],
-                              ),
-                            );
-                          },
-                        );
-                      } else if(snapshot.hasError) {
-                        return Center(child: Text(snapshot.error.toString()),);
-                      }
-                      return const CircularProgressIndicator();
-                    },
+                                    Text(Converter.currencyIndonesia(quantity[index].qty * (snapshot.data!.orderDetail[index].price ?? 0)))
+                                  ],
+                                ),
+                              );
+                            },
+                          );
+                        } else if(snapshot.hasError) {
+                          return Center(child: Text(snapshot.error.toString()),);
+                        }
+                        return const CircularProgressIndicator();
+                      },
+                    ),
                   ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    'Total',
-                    style: defaultStyle,
-                  ),
-                  Text(
-                    Converter.currencyIndonesia(total.length > 0 ? total.reduce((value, element) => value + element) : 0),
-                    style: defaultStyle,
-                  )
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    'Bayar',
-                    style: defaultStyle,
-                  ),
-                  SizedBox(
-                    width: 100,
-                    height: 18,
-                    child: TextField(
-                      controller: pay,
-                      onEditingComplete: () {
-                        FocusScope.of(context).unfocus();
-                      },
-                      onTapOutside: (_) {
-                        FocusScope.of(context).unfocus();
-                      },
-                      onChanged: (String? value) {
-                        changeTextFieldValue();
-                      },
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(5),
-                              borderSide:
-                              const BorderSide(color: Colors.black))),
-                      keyboardType: TextInputType.number,
-                      inputFormatters: <TextInputFormatter>[
-                        FilteringTextInputFormatter.digitsOnly,
-                        CurrencyTextInputFormatter(
-                          locale: 'id',
-                          decimalDigits: 0,
-                          symbol: '',
-                        )
-                      ],
-                      style: const TextStyle(fontSize: 13),
-                      textAlign: TextAlign.right,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Total',
+                      style: defaultStyle,
                     ),
-                  )
-                ],
-              ),
-            ],
+                    Text(
+                      Converter.currencyIndonesia(total.length > 0 ? total.reduce((value, element) => value + element) : 0),
+                      style: defaultStyle,
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Bayar',
+                      style: defaultStyle,
+                    ),
+                    SizedBox(
+                      width: 100,
+                      height: 18,
+                      child: TextField(
+                        controller: pay,
+                        onEditingComplete: () {
+                          FocusScope.of(context).unfocus();
+                        },
+                        onTapOutside: (_) {
+                          FocusScope.of(context).unfocus();
+                        },
+                        onChanged: (String? value) {
+                          changeTextFieldValue();
+                        },
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5),
+                                borderSide:
+                                const BorderSide(color: Colors.black))),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.digitsOnly,
+                          CurrencyTextInputFormatter(
+                            locale: 'id',
+                            decimalDigits: 0,
+                            symbol: '',
+                          )
+                        ],
+                        style: const TextStyle(fontSize: 13),
+                        textAlign: TextAlign.right,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(onPressed: () {
         widget.service.updateOrder(quantity, int.parse(pay.value.text.split('.').join()), widget.code).then((value){
-          print("Success");
+          Navigator.popAndPushNamed(context, '/selling');
         });
       }, icon: const Icon(Icons.save), backgroundColor: Colors.blueAccent, label: const Text('Simpan'),),
     );
